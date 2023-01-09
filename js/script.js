2b8b1f523ee6c81f65c84652d721f82931f92e9b
PinoMacri <pinonegozio1@outlook.it>
2023-01-09 16:04:51 +0100

    Bonus inserimento immagine aggiunto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const app = Vue.createApp({
     data(){
-        return {message: "Ciao Mondo!!"}
+        return {
+            message: "Ciao Mondo!!" ,
+            pic: "https://api.lorem.space/image/movie?w=150&amp;amp;amp;amp;h=220"
+        }
     }
 })
 
